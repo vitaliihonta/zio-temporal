a638a0cc614b80fee55316e48156e820ba9d64b0
Vitalii Honta <vitalii.honta@gmail.com>
2022-08-23 14:49:43 +0200

    Added more tests (child workflows, zio activity)

---
+++ integration-tests/src/test/scala/zio/temporal/fixture/ZioWorkflow.scala
@@ -0,0 +1,50 @@
+package zio.temporal.fixture
+
+import zio.*
+import zio.temporal.*
+import zio.temporal.activity.*
+import zio.temporal.saga.*
+import zio.temporal.state.ZWorkflowState
+import zio.temporal.workflow.*
+
+@activityInterface
+trait ZioActivity {
+  def echo(what: String): String
+}
+
+class ZioActivityImpl(implicit options: ZActivityOptions[Any]) extends ZioActivity {
+  override def echo(what: String): String =
+    ZActivity.run {
+      ZIO
+        .log(s"Echo message=$what")
+        .as(s"Echoed $what")
+    }
+}
+
+@workflowInterface
+trait ZioWorkflow {
+  @workflowMethod
+  def echo(what: String): String
+
+  @signalMethod
+  def complete(): Unit
+}
+
+class ZioWorkflowImpl extends ZioWorkflow {
+  private val state = ZWorkflowState.empty[Unit]
+
+  private val activity = ZWorkflow
+    .newActivityStub[ZioActivity]
+    .withStartToCloseTimeout(5.seconds)
+    .build
+
+  override def echo(what: String): String = {
+    val msg = activity.echo(what)
+    println("Waiting for completion...")
+    ZWorkflow.awaitWhile(state.isEmpty)
+    msg
+  }
+
+  override def complete(): Unit =
+    state := ()
+}
