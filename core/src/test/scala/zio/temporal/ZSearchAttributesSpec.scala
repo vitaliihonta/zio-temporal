f0fe838c22a11709b99f91092c881a6e6803deec
Vitalii Honta <vitalii.honta@gmail.com>
2023-08-03 21:58:51 +0200

    more tests

--- core/src/test/scala/zio/temporal/ZSearchAttributesSpec.scala
+++ core/src/test/scala/zio/temporal/ZSearchAttributesSpec.scala
@@ -21,10 +21,22 @@ class ZSearchAttributesSpec extends AnyWordSpec with Matchers {
           .build()
       )
 
+      attrs.size shouldEqual 4
+
+      attrs.containsKey[String, Plain]("foo") shouldEqual true
       attrs.get[String, Plain]("foo") shouldEqual Some("bar")
+
+      attrs.containsKey[Long, Plain]("baz") shouldEqual true
       attrs.get[Long, Plain]("baz") shouldEqual Some(42L)
+
+      attrs.containsKey[String, Keyword]("boo") shouldEqual true
       attrs.get[String, Keyword]("boo") shouldEqual Some("bzzzz")
+
+      attrs.containsKey[List[String], Keyword]("goods") shouldEqual true
       attrs.get[List[String], Keyword]("goods") shouldEqual Some(List("pizza", "burger"))
+
+      attrs.containsKey[Set[String], Keyword]("missing") shouldEqual false
+      attrs.get[Set[String], Keyword]("missing") shouldEqual None
     }
   }
 }
