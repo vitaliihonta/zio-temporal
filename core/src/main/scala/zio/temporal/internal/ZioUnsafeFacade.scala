3a69a82cfb86c9e4883ef908a3dfd8fb14916490
Ori Dagan <ori@avalor.io>
2023-11-23 12:42:27 +0200

    handle interruption cause

--- core/src/main/scala/zio/temporal/internal/ZioUnsafeFacade.scala
+++ core/src/main/scala/zio/temporal/internal/ZioUnsafeFacade.scala
@@ -4,30 +4,29 @@ import zio._
 
 object ZioUnsafeFacade {
 
-  def unsafeRunAsyncZIO[R, E, A](
-    runtime:   Runtime[R],
-    action:    ZIO[R, E, A]
-  )(onDie:     Throwable => Unit,
-    onFailure: E => Unit,
-    onSuccess: A => Unit
-  ): Unit =
+  def unsafeRunAsyncZIO[R, E, A](runtime: Runtime[R],
+                                  action: ZIO[R, E, A]
+                                )(onDie: Throwable => Unit,
+                                  onFailure: E => Unit,
+                                  onSuccess: A => Unit
+                                ): Unit =
     Unsafe.unsafe { implicit unsafe: Unsafe =>
-      // Handle defects to avoid noisy error logs
-      val errorsHandled: ZIO[R, Either[Throwable, E], A] = action
-        .mapError(Right(_))
-        .catchAllDefect(defect => ZIO.fail(Left(defect)))
-
-      val fiber = runtime.unsafe.fork(errorsHandled)
+      val fiber = runtime.unsafe.fork(action)
       fiber.unsafe.addObserver {
         case Exit.Failure(cause) =>
-          cause.failureOption.get match {
-            case Left(defect)   => onDie(defect)
-            case Right(failure) => onFailure(failure)
-          }
-
+          cause.isInterrupted
+          cause.failureOrCause.fold(
+            onFailure,
+            {
+              case Cause.Die(t, _) => onDie(t)
+              case _ => onDie(new InterruptedException())
+            }
+          )
         case Exit.Success(value) => onSuccess(value)
       }
+      runtime.unsafe.fork(fiber.interrupt)
     }
+
 
   def unsafeRunZIO[R, E, A](
     runtime:       Runtime[R],
