66058753557f8ae891b63c8ecf24db1b0e429acb
Ori Dagan <ori@avalor.io>
2023-11-24 14:42:13 +0200

    handle interruption cause (#139)
    * handle interruption cause
    
    * search for internal Die and reformat code
    
    * redundant line
    
    * redundant line

--- core/src/main/scala/zio/temporal/internal/ZioUnsafeFacade.scala
+++ core/src/main/scala/zio/temporal/internal/ZioUnsafeFacade.scala
@@ -12,19 +12,13 @@ object ZioUnsafeFacade {
     onSuccess: A => Unit
   ): Unit =
     Unsafe.unsafe { implicit unsafe: Unsafe =>
-      // Handle defects to avoid noisy error logs
-      val errorsHandled: ZIO[R, Either[Throwable, E], A] = action
-        .mapError(Right(_))
-        .catchAllDefect(defect => ZIO.fail(Left(defect)))
-
-      val fiber = runtime.unsafe.fork(errorsHandled)
+      val fiber = runtime.unsafe.fork(action)
       fiber.unsafe.addObserver {
         case Exit.Failure(cause) =>
-          cause.failureOption.get match {
-            case Left(defect)   => onDie(defect)
-            case Right(failure) => onFailure(failure)
-          }
-
+          cause.failureOrCause.fold(
+            onFailure,
+            _.find { case Cause.Die(t, _) => t }.map(onDie).getOrElse(onDie(new InterruptedException()))
+          )
         case Exit.Success(value) => onSuccess(value)
       }
     }
